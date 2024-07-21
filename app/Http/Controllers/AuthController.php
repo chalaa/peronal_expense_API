2cb7188b3809fbdf262790b5cc1f29259830dd8a
chalaa <chalaolanigeleta@gmail.com>
2024-07-21 22:07:43 +0300

    Add Authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,100 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use PHPOpenSourceSaver\JWTAuth\Contracts\Providers\JWT;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use PHPOpenSourceSaver\JWTAuth\JWTAuth as JWTAuthJWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    // register the User
+    public function register(Request $request){
+        $request->validate([
+            'name' => ['required','string'],
+            'email'=> ['required','string','email'],
+            'password'=> ['required','string','confirmed','min:8']
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+
+        // Authenticate user
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'status'=> 'success',
+            'user'=> $user,
+            'authorisation' =>  [
+                'token'=> $token,
+                'type' => 'bearer'
+            ]
+        ],200);
+
+    }
+
+    // login
+    public function login(Request $request){
+        // validate the request
+        $request->validate([
+            "email"=> ["required","email"],
+            "password"=> ["required","string",'min:8'],
+        ]);
+        
+        $credential = $request->only('email','password');
+
+        if (!Auth::attempt($credential)){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ],401);
+        }
+
+        // Authenticate user
+        $user = Auth::user();
+        $token = JWTAuth::fromUser($user);
+        return response()->json([
+            'status'=> 'success',
+            'user'=> $user,
+            'authorisation' =>  [
+                'token'=> $token,
+                'type' => 'bearer'
+            ]
+        ],200);
+    }
+
+    // logout
+
+    public function logout(){
+        Auth::logout();
+        return response()->json([
+            'status'=> 'success',
+            'message'=> 'Successfully logged out'
+        ]);
+    }
+
+    /*
+    
+    Refresh method
+    This method invalidates the user Auth 
+    token and generates a new token
+
+    */
+
+    public function refresh()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class,'register'])->name('register_user');
+Route::post('/login', [AuthController::class,'login'])->name('login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+    Route::get('/refresh',[AuthController::class,'refresh'])->name('refresh');
+});
